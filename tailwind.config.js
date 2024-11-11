99e85cd3d99fc9b10171ec82ff6bec7a29d88b49
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-11 15:26:43 +0100

    fontFamily added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        paleSand: '#F4F0D8',
-        charcoal: '#333333',
+        paleSand: "#F4F0D8",
+        charcoal: "#333333",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"], // for body text
+        Playfair: ["Playfair Display", "serif"], // for headings
       },
     },
   },
   plugins: [],
-}
+};
