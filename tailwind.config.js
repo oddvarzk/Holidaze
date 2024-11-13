1511e3f1b5a93da6d5f469873c1c232d1106564c
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-13 16:59:54 +0100

    added more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        paleSand: "#F4F0D8",
+        paleSand: "##F5F5F0",
         charcoal: "#333333",
+        tin: "#015D67",
+        tiner: "#004850",
+        btns: "#008897",
       },
       fontFamily: {
         Montserrat: ["Montserrat", "sans-serif"], // for body text
         Playfair: ["Playfair Display", "serif"], // for headings
+        Metamorphous: ["Metamorphous", "serif"], // for logo
       },
     },
   },
