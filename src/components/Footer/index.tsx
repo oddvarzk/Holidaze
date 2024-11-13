d8ce1e9811de9b5c033c79372fb47cbdeaa98024
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-13 17:00:13 +0100

    Footer setup

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
   return (
-    <div>
-      <div>
-        <p>Holidaze</p>
+    <div className="flex justify-center bg-tiner h-16">
+      <div className="flex items-center">
+        <p className="font-Metamorphous text-amber-100">Holidaze</p>
       </div>
       <div></div>
     </div>
