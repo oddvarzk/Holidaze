376687ba2cc9812ed0ec5f19e20ca107c96917be
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-15 01:57:18 +0100

    more progress on footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,12 @@
 export function Footer() {
   return (
-    <div className="flex justify-center bg-tiner h-16">
+    <div className="flex justify-center bg-tiner h-16 gap-10">
       <div className="flex items-center">
         <p className="font-Metamorphous text-amber-100">Holidaze</p>
       </div>
-      <div></div>
+      <div className="flex items-center">
+        <p>Icons</p>
+      </div>
     </div>
   );
 }
