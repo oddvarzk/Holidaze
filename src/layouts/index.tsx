0a7b5a68e0a37bca9c75774ef84a71673741a581
oddvar zakarias kristiansen <oddvarzk@gmail.com>
2024-11-15 14:01:33 +0100

    layout edited

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,6 +5,9 @@ import Footer from "../components/Footer";
 export function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
+        <div className='bg-tiner h-8 px-5 flex justify-end items-center'>
+          <p className='font-semibold text-white text-sm'>Contact</p>
+        </div>
       <Header />
       <main className="flex-grow">
         <Outlet />
