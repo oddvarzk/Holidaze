43606d1fdda9ffcb504ddebc8263d931a04f9b14
oddvar zakarias kristiansen <oddvarzk@gmail.com>
2024-11-15 14:02:34 +0100

    bg for home customized, heading changer setup

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,30 @@
+import { useState, useEffect } from 'react';
+import greeceBackg from '../../assets/greeceBackg.jpg';
+
 export function Home() {
+  const words = ['Tranquility.', 'Relax.', 'Adventure.', 'Explore.', 'Peace.'];
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [words.length]);
+
   return (
-    <div>
-      <h1 className="text-center">Testing</h1>
+    <div
+      className="bg-no-repeat bg-[position:20%_60%] bg-[length:100%] h-[40vh] w-full flex items-center justify-center"
+      style={{ backgroundImage: `url(${greeceBackg})` }}
+    >
+      <div className="text-center">
+        <h1 className="text-4xl font-Montserrat font-normal text-white animate-fadeIn">{words[currentWordIndex]}</h1>
+      </div>
     </div>
   );
 }
 
 export default Home;
+
+
