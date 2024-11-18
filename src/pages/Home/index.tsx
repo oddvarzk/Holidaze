7075b5499fb0464b6e7a223d02065b1b56a6ebb2
oddvar zakarias kristiansen <oddvarzk@gmail.com>
2024-11-18 17:13:33 +0100

    Added div for other sections

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,15 +14,18 @@ export function Home() {
   }, [words.length]);
 
   return (
-<div className="relative bg-no-repeat bg-cover h-[50vh] w-full flex items-center justify-center bg-top sm:bg-center"
-     style={{ backgroundImage: `url(${greeceBackg})` }}>
-  <div className="absolute inset-0 bg-black bg-opacity-30"></div>
-  <div className="w-full h-full flex justify-center items-center">
-    <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-Montserrat font-normal text-white animate-pulse">
-      {words[currentWordIndex]}
-    </h1>
-  </div>
-</div>
+    <div>
+      <div className="relative bg-no-repeat bg-cover h-[50vh] w-full flex items-center justify-center bg-top sm:bg-center"
+          style={{ backgroundImage: `url(${greeceBackg})` }}>
+        <div className="absolute inset-0 bg-black bg-opacity-30"></div>
+        <div className="w-full h-full flex justify-center items-center">
+          <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-Montserrat font-normal text-white animate-pulse">
+            {words[currentWordIndex]}
+          </h1>
+        </div>
+      </div>
+    </div>
+
 
 
   );
