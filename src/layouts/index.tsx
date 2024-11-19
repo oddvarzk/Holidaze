3b6dbe03e95c97d778b8b2405b29032ebe4d2b44
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-19 03:29:26 +0100

    layout finished and eng icon added

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,13 +1,22 @@
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import unitedKingdomIcon from "../assets/unitedKingdom.svg";
 
 export function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
-        <div className='bg-tiner h-8 px-5 flex justify-end items-center'>
-          <p className='font-semibold text-white text-sm'>Contact</p>
+      <div className="bg-tiner gap-5 h-8 px-5 flex justify-end items-center">
+        <p className="font-semibold text-paleSand text-sm">Contact us</p>
+        <div className="flex">
+          <img
+            src={unitedKingdomIcon}
+            alt="United Kingdom Flag Icon"
+            className="h-6 w-6"
+          />
+          <p className="px-2 text-paleSand">EN</p>
         </div>
+      </div>
       <Header />
       <main className="flex-grow">
         <Outlet />
