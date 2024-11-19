3bf26ed3bf9d23162f4b27badf4fee3332e87ac9
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-19 04:41:51 +0100

    Link to Contact added

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,12 +2,15 @@ import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import unitedKingdomIcon from "../assets/unitedKingdom.svg";
+import { Link } from "react-router-dom";
 
 export function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="bg-tiner gap-5 h-8 px-5 flex justify-end items-center">
-        <p className="font-semibold text-paleSand text-sm">Contact us</p>
+        <p className="font-semibold text-paleSand text-sm">
+          <Link to="/Contact">Contact us</Link>
+        </p>
         <div className="flex">
           <img
             src={unitedKingdomIcon}
