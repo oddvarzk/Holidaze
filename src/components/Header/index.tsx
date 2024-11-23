275fee20cf57a8cae2076ebb43c2c02272935222
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-23 16:28:27 +0100

    header spacing fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export function Header() {
   return (
     <header className="flex justify-between py-2 px-7 bg-tin text-white">
-      <div>
+      <div className="mt-2">
         <Link to="/">
           <h3 className="font-Metamorphous text-xl text-amber-100 py-6">
             Holidaze
