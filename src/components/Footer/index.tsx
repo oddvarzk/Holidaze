fc242cb61fcff97c54624a9e21e24ae7124221d7
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-28 09:47:59 +0100

    mt top on footer increased for spacing

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div className="flex justify-center bg-tiner h-16 gap-10">
+    <div className="flex justify-center bg-tiner mt-10 h-16 gap-10">
       <div className="flex items-center">
         <p className="font-Metamorphous text-amber-100">Holidaze</p>
       </div>
