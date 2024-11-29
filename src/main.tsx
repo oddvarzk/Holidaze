9fb30cb73df339737d731d3a589a111a16e36330
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-29 19:18:10 +0100

    AuthProvider wrapping app

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,19 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import { AuthProvider } from "./components/context/authContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
