5829fcf641d94bc9196671005d2b39597cfd1aa6
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-29 19:36:13 +0100

    app was double routed, error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// src/App.tsx
+
+import { Routes, Route } from "react-router-dom"; // Removed BrowserRouter import
 import { Layout } from "./layouts";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
@@ -7,7 +9,6 @@ import LoginForm from "./pages/auth/Login";
 import MyProfile from "./pages/user/myProfile";
 import VenueList from "./pages/venues";
 import About from "./pages/about";
-import { AuthProvider } from "./components/context/authContext.tsx";
 import ProtectedRoute from "./components/ProtectedRoute";
 import CreateVenuePage from "./components/CreateVenuePage";
 import UpdateVenuePage from "./pages/venues/updateVenue";
@@ -15,45 +16,43 @@ import SingleVenue from "./pages/venues/singleVenue";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="register" element={<RegisterForm />} />
-            <Route path="login" element={<LoginForm />} />
-            <Route
-              path="profile"
-              element={
-                <ProtectedRoute>
-                  <MyProfile />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="venues" element={<VenueList />} />
-            <Route path="about" element={<About />} />
-            <Route path="venue/:id" element={<SingleVenue />} />
-            <Route
-              path="createVenue"
-              element={
-                <ProtectedRoute>
-                  <CreateVenuePage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="updateVenue/:id"
-              element={
-                <ProtectedRoute>
-                  <UpdateVenuePage />
-                </ProtectedRoute>
-              }
-            />
-          </Route>
-        </Routes>
-      </Router>
-    </AuthProvider>
+    // Removed <AuthProvider> as it's already in main.tsx
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="register" element={<RegisterForm />} />
+        <Route path="login" element={<LoginForm />} />
+        <Route
+          path="profile"
+          element={
+            <ProtectedRoute>
+              <MyProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="venues" element={<VenueList />} />
+        <Route path="about" element={<About />} />
+        <Route path="venues/:id" element={<SingleVenue />} />{" "}
+        {/* Changed to plural for consistency */}
+        <Route
+          path="createVenue"
+          element={
+            <ProtectedRoute>
+              <CreateVenuePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="updateVenue/:id"
+          element={
+            <ProtectedRoute>
+              <UpdateVenuePage />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
+    </Routes>
   );
 }
 
