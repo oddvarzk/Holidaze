28e3357abdd490b7e2104b13a39ddaefca39b907
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-29 22:52:51 +0100

    Protected Route useAuth setup

--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -1,30 +1,20 @@
 // src/components/ProtectedRoute.tsx
 
 import React from "react";
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/authContext.tsx"; // Ensure correct import path
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../../components/context/authContext.tsx"; // Adjust the import path
 
 interface ProtectedRouteProps {
   children: JSX.Element;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { isAuthenticated, role } = useAuth();
+  const { isAuthenticated } = useAuth();
+  const location = useLocation();
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
-  }
-
-  if (!role) {
-    // Assuming role=true means venueManager
-    return (
-      <div className="py-5 px-5 mt-5 max-w-4xl mx-auto">
-        <div className="bg-red-100 text-red-700 p-4 rounded">
-          <h2 className="text-2xl font-semibold">Access Denied</h2>
-          <p>You do not have the necessary permissions to access this page.</p>
-        </div>
-      </div>
-    );
+    // Redirect unauthenticated users to the login page
+    return <Navigate to="/login" replace state={{ from: location }} />;
   }
 
   return children;
