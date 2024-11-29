d8d686eaf587d8b28db1a5d4cc58fc0f89680a4e
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-29 23:30:24 +0100

    types for user added in seperate folder

---
+++ src/types/User/index.tsx
@@ -0,0 +1,20 @@
+// src/types/User.tsx
+
+export interface Avatar {
+  url: string;
+  alt: string;
+}
+
+export interface Banner {
+  url: string;
+  alt: string;
+}
+
+export interface User {
+  name: string;
+  email: string;
+  bio?: string;
+  avatar?: Avatar;
+  banner?: Banner;
+  venueManager: boolean;
+}
