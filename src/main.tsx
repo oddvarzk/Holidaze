decce9226ea3a67108a888f4f132286b98c31cbc
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-11-29 23:28:19 +0100

    AuthProvider renamed

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
-import { AuthProvider } from "./components/context/authContext.tsx";
+import { AuthProvider } from "./components/context/authContext/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
