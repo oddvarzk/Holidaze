7098290ca1c5a5812872ccc3b3f1091dd5a5f9e4
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-12-01 16:16:35 +0100

    Added currentYear to footer.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,12 @@
 export function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="flex justify-center bg-tiner h-16 gap-10">
       <div className="flex items-center">
-        <p className="font-Metamorphous text-amber-100">Holidaze</p>
+        <p className="font-Metamorphous text-amber-100">
+          © {currentYear} Holidaze
+        </p>
       </div>
     </div>
   );
